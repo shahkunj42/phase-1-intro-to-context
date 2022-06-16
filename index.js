2544abf5548c9a27e3f15bb033ce6483d26813df
Kunj <shahkunj42@gmail.com>
2022-06-16 15:31:53 -0400

    commit tree db0715976e9ad8677be698f8420d7705bc6bdc59

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function createEmployeeRecord(array){
+    return{
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(array) {
+    return array.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+function createTimeInEvent(employeeRecord, string){
+    let array = string.split(' ')
+    employeeRecord.timeInEvents.push({
+        type: 'TimeIn',
+        date: array[0],
+        hour: parseInt(array[1])
+    })
+    return employeeRecord
+}
+
+function createTimeOutEvent(employeeRecord, string){
+    let array = string.split(' ')
+    employeeRecord.timeOutEvents.push({
+        type: 'TimeOut',
+        date: array[0],
+        hour: parseInt(array[1])
+    })
+    return employeeRecord
+}
